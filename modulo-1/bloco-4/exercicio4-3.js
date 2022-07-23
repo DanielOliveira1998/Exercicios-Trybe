d4ca277af4cd2acf91150bdf2ee1a84e14ddf805
Daniel Oliveira <danoliveira865@gmail.com>
2022-07-23 01:49:29 -0300

    tentando resolver a quarta questão

---
+++ modulo-1/bloco-4/exercicio4-3.js
@@ -0,0 +1,29 @@
+// criando o conjunto de 2 a 50.
+// let conjuntoNumerico = [];
+// for (let index = 2; index < 51; index+=1){
+//     conjuntoNumerico.push(index);
+// }
+// console.log("Conjunto numérico da questão:", conjuntoNumerico);
+
+// let conjuntoFatoracao = [];
+// for (let index2 = 1; index2 < 10; index2+=1){
+//     conjuntoFatoracao.push(index2);
+// }
+// console.log("Conjunto verfificador da hipósete do número ser primo:", conjuntoFatoracao);
+// let i = 0;
+// let i2 = 0;
+// let numerosPrimos = [];
+// for(i = 0; i < 49; i += 1){
+//     for(i2 = 0;conjuntoFatoracao[i2]===conjuntoNumerico[i]; i2 += 1){
+//         if(conjuntoNumerico[i]%conjuntoFatoracao[i2] != 0){
+//             numerosPrimos.push(i);
+//         }
+//     }
+// }
+// console.log(numerosPrimos);
+// console.log("Esse é o maior número primo", maiorNumeroPrimo);
+
+let arraya = [50, 10, 20];
+let arrayB = [2, 3, 4];
+console.log(arraya+arrayB);
+
